6ec4a02e19bd4ebced473ff425b6a372b5c2dace
William-Fernandes252 <william.winchester1967@gmail.com>
2022-05-10 10:24:21 -0300

    Big Bang

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Pig-Game
+
 A simple two-player game implemented in Javascript with JQuery.
+
+![Game](./snapshot/game.jpg)
+
+## Rules
+
+Roll the dice to accumulate points;
+
+        Hold to retrieve the points
+        and pass the turn to the other player;
+
+        If the top face is one, the active player loses his/her
+        accumulated points;
+
+        The first player to get
+        100 points wins the game 🏆!
+
+</ol>
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/script/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
